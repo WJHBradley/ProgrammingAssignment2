a2d56a15eda743d23d529de3c82d929d013e6dfa
WJHBradley <wbradley@thevisioncouncil.org>
2019-05-30 14:14:09 -0400

    Completed programming assignment
    makeCacheMatrix can now accept an input Matrix, and creates an environment with four functions
    
    cacheSolve takes the inverse of the makeCacheMatrix, and stores the inverse in the cache environment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions store the inverse of a matrix in cache 
+## 
 
-## Write a short comment describing this function
+## This function takes an input matrix for use with the cacheSolve function.
+## It returns a list of four functions, which is required for input into cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv = NULL
+      setMatrix <- function(y){
+        x <<- y
+        inv <<- NULL
+      }
+      getMatrix <- function() x
+      setInverse <- function(solve) inv <<- solve
+      getInverse <- function() inv
+      list(setMatrix = setMatrix, getMatrix = getMatrix, 
+           setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function will cache and return an inverse of the matrix 'x'
+## Similar to cachemean, it checks to see if the inverse is cached before
+## solving and returning the inverse, inv.
+
+## Also, the input 'x' must be a list output from makeCacheMatrix
+## in order to function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        if(!is.null(inv)){
+          message("getting cached data")
+          return(inv)
+        }
+        Mdata <- x$getMatrix()
+        inv <- solve(Mdata,...)
+        x$setInverse(inv)
+        inv
+      
 }
